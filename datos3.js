04f3815c6dedbc32e9af1d653cc44870089702a7
Jose-BE <1513167@senati.pe>
2025-04-08 17:21:42 -0500

    datos-jose

---
+++ datos3.js
@@ -0,0 +1,11 @@
+const usuario = {
+    id: 3,
+    nombre: 'Jose',
+    apellido: 'Bravo Espinoza',
+    carrera: 'Ingeniería de Software con IA',
+    semestre: 5,
+    email: '1513167@senati.pe',
+    telefono: '982848321',
+    dni: '60769894',
+    edad: 18
+};
