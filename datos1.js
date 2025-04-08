b5cac5d5053bf6192558bc0e77fe93de145398d3
Edgar-BC <1515047@senati.pe>
2025-04-08 17:13:39 -0500

    datos1- edgar

---
+++ datos1.js
@@ -0,0 +1,11 @@
+const usuario = {
+    id: 1,
+    nombre: 'Edgar',
+    apellido: 'Bazan Cardenas',
+    carrera: 'Ingeniería de Software con IA',
+    semestre: 5,
+    email: '1515047@senati.pe',
+    telefono: '903447383',
+    dni: '76334620',
+    edad: 19
+};
