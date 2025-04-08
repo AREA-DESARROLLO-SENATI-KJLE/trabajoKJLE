f620d6258c95900def1b8fc8cf413ac1b42e4f8b
KimberlyHC31 <1529022@senati.pe>
2025-04-08 16:58:55 -0500

    Avance del index

---
+++ datos.js
@@ -0,0 +1,11 @@
+const usuario = {
+    id: 1,
+    nombre: 'Kimberly',
+    apellido: 'Huamán Cristóbal',
+    carrera: 'Ingeniería de Software con IA',
+    semestre: 5,
+    email: 'kimberlyhuamancris31@gmail.com',
+    telefono: '927631223',
+    dni: '60784472',
+    edad: 19
+};
