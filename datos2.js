5e76d867ed14b5bbe171606b2bf54f3f7f19be9c
LeticiaBazanC <1522577@senati.pe>
2025-04-08 17:16:54 -0500

    Avance datos

---
+++ datos2.js
@@ -0,0 +1,11 @@
+const usuario = {
+    id: 2,
+    nombre: 'Leticia',
+    apellido: 'Bazan Caballero',
+    carrera: 'Ingeniería de Software con IA',
+    semestre: 5,
+    email: '1522577@senati.pe',
+    telefono: '969787276',
+    dni: '74155223',
+    edad: 19
+};
